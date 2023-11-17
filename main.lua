0ad08b71b45afcc6c46d89e6e09a44fd858a1dcb
Nitroxyz <vladhust@gmail.com>
2023-11-17 01:19:31 +0100

    Added functions

--- main.lua
+++ main.lua
@@ -1,11 +1,11 @@
 meta = {
     name = "Lazarus Ranked",
-    version = "0.3",
+    version = "0.4",
     author = "Nitroxy",
     description = ""
 }
 
---4
+--5
 
 local prev_option = 1;
 
@@ -18,7 +18,6 @@ local saved_data = {
 
 set_callback(function (save_ctx)
     local save_data_str = json.encode(saved_data);
-    print(inspect(save_data_str));
     save_ctx:save(save_data_str);
 end, ON.SAVE)
 
@@ -27,35 +26,33 @@ set_callback(function(load_ctx)
     print(inspect(load_data_str));
     if load_data_str ~= "" then
         saved_data = json.decode(load_data_str);
-        options.b_penalty = saved_data.penalty;
+        load_penalty();
     end
 end, ON.LOAD)
 
 set_callback(function ()
     -- Save one last time
-    saved_data.penalty = options.a_penalty;
-    save_progress();
+    update_penalty();
     -- Reset penalty
     stuff.set_penalty(30 * 60);
 end, ON.SCRIPT_DISABLE)
 
 set_callback(function ()
     if(prev_option ~= options.a_mode)then
-        if(options.a_mode == 1)then
-            options.b_penalty = saved_data.penalty
-        else
-            options.b_penalty = saved_data.sc_penalty
-        end
-        stuff.set_penalty(options.b_penalty * 60);
+        load_penalty();
         prev_option = options.a_mode;
     end
 end, ON.GUIFRAME)
 
 register_option_combo("a_mode", "Category", "", "Any%\0SunkenCity\0\0", 1);
 
-register_option_int("b_penalty", "", "", 0, 0, 0);
+register_option_int("b_penalty", "", "", 30, 0, 0);
 
 register_option_button("c_update", "Update penalty", "", function ()
+    update_penalty();
+end)
+
+function update_penalty()
     if(options.a_mode == 1)then
         saved_data.penalty = options.b_penalty;
     else
@@ -63,4 +60,14 @@ register_option_button("c_update", "Update penalty", "", function ()
     end
     stuff.set_penalty(options.b_penalty * 60);
     save_progress();
-end)
+    print("Updated!");
+end
+
+function load_penalty()
+    if(options.a_mode == 1)then
+        options.b_penalty = saved_data.penalty
+    else
+        options.b_penalty = saved_data.sc_penalty
+    end
+    stuff.set_penalty(options.b_penalty * 60);
+end
